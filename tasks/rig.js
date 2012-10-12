381fcc312ccf7c24a07964ab61ba61ffb18a4a47
Damon Oehlman <damon.oehlman@sidelab.com>
2012-10-12 15:37:49 +1000

    Carried across functionality from the original grunt-rigger implementation

--- tasks/rig.js
+++ tasks/rig.js
@@ -28,31 +28,14 @@ function logOutput(instance) {
   
 // initialise the compile helper
 helpers.compile = function() {
-    var files = [],
-        done = this.async();
+    var files = [];
 
     // iterate through the files 
     _.forEach(this.data, function(src, dst) {
         files.push({ src: src, dst: dst });
     });
     
-    async.forEach(
-        files,
-        function(file, itemCallback) {
-            var destFile = path.resolve(file.dst),
-                opts = _.defaults(riggerOpts, {
-                    targetType: path.extname(destFile)
-                });
-            
-            logOutput(rigger(path.resolve(file.src), opts, function(err, output) {
-                if (err) return itemCallback(err);
-                
-                grunt.file.mkdir(path.dirname(destFile));
-                fs.writeFile(destFile, output, 'utf8', itemCallback);
-            }));
-        },
-        done
-    );
+    async.forEach(files, rigTarget, this.async());
 };
     
 function rigFiles() {
@@ -62,6 +45,61 @@ function rigFiles() {
     
     // call the appropriate helper
     (helpers[this.target] || helpers.compile).call(this);
+}
+
+function rigTarget(target, callback) {
+    // expand the directives
+    var destFile = path.resolve(target.dst),
+        files = [].concat(target.src || []),
+        fileOpts = files.map(function(filepath) {
+            var directive = grunt.task.getDirectiveParts(filepath);
+        
+            // if the directive is a file directive, then extract the basepath
+            if (directive && reFileDirective.test(directive[0])) {
+                filepath = directive[1];
+            }
+            // otherwise if we are dealing with a directive, then reset the filepath
+            else if (directive) {
+                filepath = '';
+            }
+        
+            // return the directory for the path
+            return filepath ? {
+                cwd: path.resolve(path.dirname(filepath)),
+                targetType: path.extname(filepath)
+            } : null;
+        }),
+        
+        // get the file contents
+        fileContents = files.map(function(filepath) {
+          return grunt.task.directive(filepath, grunt.file.read);
+        }),
+        
+        opts = _.defaults(riggerOpts, {
+            separator: grunt.utils.linefeed
+        }),
+        
+        // initialise the file tracking index
+        fileIndex = 0;
+        
+    // process each of the files that need to be rigged
+    async.map(
+        fileContents,
+        
+        function(data, itemCallback) {
+            logOutput(rigger.process(data, _.defaults(fileOpts[fileIndex++], riggerOpts), itemCallback));
+        },
+    
+        function(err, results) {
+            grunt.file.mkdir(path.dirname(destFile));
+            
+            fs.writeFile(
+                destFile,
+                results.join(grunt.utils.normalizelf(opts.separator)),
+                'utf8',
+                callback);
+        }
+    );
 }
 
 module.exports = function(grunt) {
